c383713cf42b6554ab43182fb5227dbcf1413998
Liu Junyu <131525965+s120nxn@users.noreply.github.com>
2023-09-10 16:46:46 +0800

    Update Testss.java

--- src/com/hs/Testss.java
+++ src/com/hs/Testss.java
@@ -3,5 +3,6 @@ package com.hs;
 public class Testss {
     public static void main(String[] args) {
         System.out.println("hello gitee");
+        System.out.println("hello github");
     }
 }
